49e006c538d8a12af029117d008b6c81cdb652f3
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-05-13 14:02:48 -0300

    checkout file

---
+++ src/services/CheckoutServices.ts
@@ -0,0 +1,101 @@
+import { Customer, Order, PrismaClient } from "@prisma/client"
+
+import { CustomerData } from "../interfaces/CustomerData"
+import { PaymentData } from "../interfaces/PaymentData"
+import { SnackData } from "../interfaces/SnackData"
+import PaymentService from "./PaymentService"
+
+export default class CheckoutService {
+  private prisma: PrismaClient
+
+  // metodo de inicialização/construção
+  constructor() {
+    this.prisma = new PrismaClient()
+  }
+
+  async process(
+    cart: SnackData[],
+    customer: CustomerData,
+    payment: PaymentData
+  ) {
+    //? "puxar" os dados de snacks do BD
+    const snacks = await this.prisma.snack.findMany({
+      where: {
+        id: {
+          in: cart.map((snack) => snack.id),
+        },
+      },
+    })
+    // console.log(`snacks`, snacks)
+
+    const snacksInCart = snacks.map<SnackData>((snack) => ({
+      ...snack,
+      price: Number(snack.price),
+      quantity: cart.find((item) => item.id === snack.id)?.quantity!,
+      subTotal:
+        cart.find((item) => item.id === snack.id)?.quantity! *
+        Number(snack.price),
+    }))
+    // console.log(`snackInCart`, snackInCart)
+
+    //? registrar os dados do cliente no BD
+    const customerCreated = await this.createCustomer(customer)
+    // console.log(`customerCreated`, customerCreated)
+
+    //? criar uma order
+    // esta como let pq podemos atribuir novamente para esta variável uma chamda de order.update
+    let orderCreated = await this.createOrder(snacksInCart, customerCreated)
+    // console.log(`orderCreated`, orderCreated)
+
+    //? processar o pagamento
+    const trasaction = await new PaymentService().process(
+      orderCreated,
+      customerCreated,
+      payment
+    )
+  }
+
+  // metodos de utilidades
+  private async createCustomer(customer: CustomerData): Promise<Customer> {
+    const customerCreated = await this.prisma.customer.upsert({
+      // procura o email do customer
+      where: { email: customer.email },
+      // se tiver o email ele atualiza todo o customer
+      update: customer,
+      // se nao tiver ele cria um customer novo
+      create: customer,
+    })
+
+    return customerCreated
+  }
+
+  private async createOrder(
+    snacksInCart: SnackData[],
+    customer: Customer
+  ): Promise<Order> {
+    const total = snacksInCart.reduce((acc, snack) => acc + snack.subTotal, 0)
+    const orderCreated = await this.prisma.order.create({
+      data: {
+        total,
+        customer: {
+          connect: { id: customer.id },
+        },
+        orderItems: {
+          createMany: {
+            data: snacksInCart.map((snack) => ({
+              snackId: snack.id,
+              quantity: snack.quantity,
+              subTotal: snack.subTotal,
+            })),
+          },
+        },
+      },
+      include: {
+        customer: true,
+        orderItems: { include: { snack: true } },
+      },
+    })
+
+    return orderCreated
+  }
+}
